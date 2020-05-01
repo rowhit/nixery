68d5d3790b9cc24f43dbc5ad2642138315498dbc
Vincent Ambo <tazjin@google.com>
2020-05-01 12:46:38 +0100

    chore(build): Change pin for default nixpkgs used to build Nixery
    This moves the pin from just being in the Travis configuration to also
    being set in a nixpkgs-pin.nix file, which makes it trivial to build
    at the right commit when performing local builds.

---
+++ nixpkgs-pin.nix
@@ -0,0 +1,4 @@
+import (builtins.fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs-channels/archive/43ab8e86ca633cd144a5a79767fcc00f305f6ef0.tar.gz";
+  sha256 = "10l4yhk32gx8a37njwc6rmd3xig6ahs7ax4ym3gkf9ckw0mmn9wm";
+}) {}
